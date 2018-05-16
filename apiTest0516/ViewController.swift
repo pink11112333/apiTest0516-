ab5c0ba2f2a3b3b7176daed97dd44514b212dba3
viplab <s104321030@mail1.ncnu.edu.tw>
2018-05-16 11:24:13 +0800

    Initial Commit

---
+++ apiTest0516/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apiTest0516
+//
+//  Created by viplab on 2018/5/16.
+//  Copyright © 2018年 viplab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
